653125073dc24a9ed204e33635c8589abf899ec5
vishwa <vishwadeep.ghimire@gmail.com>
2023-10-14 12:01:33 -0500

    common, reservation and tutor service module init

---
+++ tutor-service/src/main/java/com/example/tutorservice/TutorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorServiceApplication.class, args);
+    }
+
+}
