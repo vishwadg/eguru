c40f1b4212930f08377ff139bc13e718a8b4c21e
vishwa <vishwadeep.ghimire@gmail.com>
2023-10-14 17:13:06 -0500

    docker files

--- commons-module/src/main/java/com/example/commonmodule/DTOs/NotificationDto.java
+++ commons-module/src/main/java/com/example/commonmodule/DTOs/NotificationDto.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+/**
+ * The NotificationDto class represents a data transfer object for notification-related information.
+ * It includes fields such as the recipient, subject, and message text.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,3 +18,4 @@ public class NotificationDto {
     private String subject;
     private String text;
 }
+
